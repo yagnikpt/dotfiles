aaa2f19e7fb3bc833888efc9efa43b6e9248a4fc
Yagnik <coderisaddicted@gmail.com>
2025-12-01 14:05:16 +0530

    niri spawn to service files

--- scripts/scripts/rofi/wallpicker.sh
+++ scripts/scripts/rofi/wallpicker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wall_dirs="$HOME/Pictures/wallpapers/scifi:$HOME/Pictures/wallpapers/arc_raiders"
+wall_dirs="$HOME/Pictures/wallpapers/alt:$HOME/Pictures/wallpapers/arc_raiders"
 cache_dir="$HOME/.cache/wallpaper_thumbnails/"
 
 # Convert colon-separated string to array
